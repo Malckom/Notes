1a613937bd84461cdb23af9bcc2aec81b9b000eb
Malcom Nyaranga <kibisu14@icloud.com>
2025-01-23 00:16:02 +0300

    google auth

--- app.js
+++ app.js
@@ -3,15 +3,35 @@ require ('dotenv').config();
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const connectDB = require('./server/config/db');
+const session = require('express-session');
+const passport = require('passport');
+const MongoStore = require('connect-mongo');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+app.use(session({
+  secret: 'Pathways',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI
+  }),
+  //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+  // Date.now() - 30 * 24 * 60 * 60 * 1000
+}));
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+app.use(passport.initialize());
+// app.use(passport.session());
+
+
 // CONNECTING DB
 connectDB();
+
 
 // STATIC FILES
 app.use(express.static('public'));
@@ -22,6 +42,7 @@ app.set('layout', './layouts/main')
 app.set('view engine', 'ejs');
 
 // ROUTES
+app.use('/', require('./server/routes/auth'));
 app.use('/', require('./server/routes/index'));
 app.use('/', require('./server/routes/dashboard'));
 
